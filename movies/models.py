d90db4fbb7c5d152cb636ec50db641a6d7b0204f
James Ness <jeness@lappy.home.net>
2017-01-15 13:35:41 -0500

    Created MovieView model

--- movies/models.py
+++ movies/models.py
@@ -7,10 +7,17 @@ from django.db import models
 class Movie(models.Model):
 	uid = models.CharField(max_length=10, primary_key=True)
 	title = models.CharField(max_length=200)
-	original_title = models.CharField(max_length=200,null=True)
+	original_title = models.CharField(max_length=200,null=True,blank=True)
 	production_year = models.DateField()
 	director = models.CharField(max_length=200)
 	poster = models.URLField(blank=True)
 
 	def __str__(self):
 		return self.title
+
+class MovieViews(models.Model):
+	uid = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	view_date = models.DateField()
+
+	def __str__(self):
+		return self.view_date
