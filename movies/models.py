c48970e464c64beeec40a49b012fee4fed20e260
James Ness <jeness@lappy.home.net>
2017-01-15 13:30:08 -0500

    Create initial Movie model

--- movies/models.py
+++ movies/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+	uid = models.CharField(max_length=10, primary_key=True)
+	title = models.CharField(max_length=200)
+	original_title = models.CharField(max_length=200,null=True)
+	production_year = models.DateField()
+	director = models.CharField(max_length=200)
+	poster = models.URLField(blank=True)
+
+	def __str__(self):
+		return self.title
